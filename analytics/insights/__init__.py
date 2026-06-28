427c06350b65047312abc79f2db447e44dcec714
Gustavo Alexander Vaca Tello <gusvaca10.gvt@gmail.com>
2026-06-27 22:23:09 -0500

    Actualiza init.py

---
+++ analytics/insights/__init__.py
@@ -0,0 +1,9 @@
+from analytics.insights.engine import InsightEngine
+from analytics.insights.service import InsightService
+from analytics.insights.facade import InsightFacade
+
+__all__ = [
+    "InsightEngine",
+    "InsightService",
+    "InsightFacade",
+]
